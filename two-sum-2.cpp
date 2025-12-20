a389476d429b5aa710749266962c15432e10fcb7
AbhinavAlluvada <abhinav.alluvada@gmail.com>
2025-12-20 21:06:29 +0530

    Two Sum O(n2)

---
+++ two-sum-2.cpp
@@ -0,0 +1,33 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    vector<int> twoSum(vector<int> &nums, int target)
+    {
+        for (int i = 0; i < nums.size(); i++)
+        {
+            for (int j = i + 1; j < nums.size(); j++)
+            {
+                if (nums[i] + nums[j] == target)
+                {
+                    return {i, j};
+                }
+            }
+        }
+        return {0, 0};
+    }
+};
+
+int main()
+{
+    vector<int> nums = {2, 7, 11, 15};
+    int target = 9;
+    Solution s;
+    vector<int> ans = s.twoSum(nums, target);
+    for (int i : ans)
+        cout << i << " ";
+    return 0;
+}
