b589c8f5fcdf7e9da3ad42b89e587d803c82dc49
AbhinavAlluvada <abhinav.alluvada@gmail.com>
2025-12-22 11:07:00 +0530

    LC88

--- test.cpp
+++ test.cpp
@@ -1,11 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
+void print(const vector <int>& vect){
+    for(auto itr = vect.begin();itr!= vect.end();itr++){
+        cout << *itr << " ";
+    }
+    cout << endl;
+}
 int main(){
     vector <int> vect = {1,2,3};
     vector <int> vect2 = {4,5,6};
-    vect.insert(vect.end(),vect2.begin(),vect2.end());
-    for(int i: vect){
-        cout << i << " ";
-    }
-    cout << endl;
+    int k = vect.size()-1;
+    print(vect);
+    cout << "k: "<<k<<endl;
+
+    vect[k--] = 0;
+    print(vect);
+    cout << "k: "<<k<<endl;
+
+    return 0;
 }
