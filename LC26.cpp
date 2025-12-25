a31d63bc9f82a9a5da2347fb874dc2e64b86c93c
AbhinavAlluvada <abhinav.alluvada@gmail.com>
2025-12-25 19:02:09 +0530

    LeetCode:

--- LC26.cpp
+++ LC26.cpp
@@ -8,27 +8,25 @@ public:
     int removeDuplicates(vector<int> &nums)
     {
         // this code basically seperates all the dupliactes to the end and unique elements to the first;
-        if (nums.empty())
-        {
+        if(nums.empty()){
             return 0;
         }
-        int k = 0;
-        for (int i = 0; i < nums.size(); i++)
-        {
-            if (nums[i] != nums[k])
-            {
-                k++;
-                nums[k] = nums[i];
+        int fsptr = 0;
+        for(int i = 0;i<nums.size();i++){
+            if(nums[i] != nums[fsptr]){
+                fsptr++;
+                nums[fsptr] = nums[i];
             }
         }
-        return k + 1;
+        return fsptr + 1;
     }
 };
 
 int main()
 {
-    vector<int> nums = {1, 1, 2, 3};
+    vector<int> nums = {1,4,5,1,1, 2, 3};
     Solution s;
+    sort(nums.begin(),nums.end());
     int k = s.removeDuplicates(nums);
     for (int i = 0; i < k; i++)
     {
