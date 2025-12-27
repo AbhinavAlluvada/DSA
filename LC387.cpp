a13fd07e47a96a6cf8cbd7dff69703f27257db3f
AbhinavAlluvada <abhinav.alluvada@gmail.com>
2025-12-27 19:56:19 +0530

    LeetCode387

--- LC387.cpp
+++ LC387.cpp
@@ -4,7 +4,7 @@ using namespace std;
 class Solution {
 public:
     int firstUniqChar(string s) {
-        int arr[26] = {0};
+        int arr[26] = {0}; 
         for(char c: s){
             arr[c-'a']++;
         }
