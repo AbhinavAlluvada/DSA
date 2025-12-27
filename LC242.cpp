65f5793291b8725b570f8fac27d809a2a65fc2c0
AbhinavAlluvada <abhinav.alluvada@gmail.com>
2025-12-27 19:28:32 +0530

    LeetCode 242

---
+++ LC242.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    string s ="rat1";
+    string t = "car1";
+    if(s.size()!=t.size()){
+        cout << "Nop!"<<endl;
+        return 0;
+    }
+    unordered_map <char,int> cmap;
+    for(char c : s){
+        cmap[c]++;
+    }
+    unordered_map <char,int> dmap;
+    for(char d : t){
+        dmap[d]++;
+    }
+    if(cmap!=dmap){
+        cout << "Nope!"<<endl;
+    }
+    cout << "yep!"<<endl;
+}
