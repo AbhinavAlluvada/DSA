f8710ad44653cc12db2f611e4a5c2df537729776
AbhinavAlluvada <abhinav.alluvada@gmail.com>
2025-12-27 19:15:07 +0530

    LC125

---
+++ LC125.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    bool ispalin(string str)
+    {
+        string res;
+        for (char c : str)
+        {
+            if (isalnum(c))
+            {
+                res.push_back(tolower(c));
+            }
+        }
+        return cp(res);
+    }
+    bool cp(string &res)
+    {
+        int i = 0;
+        int j = res.size() - 1;
+        while (i < j)
+        {
+            if (res[i] != res[j])
+            {
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+};
+int main()
+{
+    Solution obj;
+    string str = "A man, a plan, a canal: Panama";
+    bool result = obj.ispalin(str);
+    if (result)
+    {
+        cout << "Palindrome" << endl;
+    }
+}
--- test.cpp
+++ test.cpp
@@ -1,19 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
-void print(const vector <int>& vect){
-    for(auto itr = vect.begin();itr!= vect.end();itr++){
+void print(const vector<int> &vect)
+{
+    for (auto itr = vect.begin(); itr != vect.end(); itr++)
+    {
         cout << *itr << " ";
     }
     cout << endl;
 }
-int main(){
-    string str;
-    getline(cin , str);
-    cout << str << endl;
-    string str2;
-    getline(cin, str2);
-    cout << str2 <<endl;
-    string str3 = str + str2;
-    cout << str3 << endl;
-    return 0;
+int main()
+{
 }
