6b73ec08d20751677507c745cc37d86a2b1a739d
AbhinavAlluvada <abhinav.alluvada@gmail.com>
2025-12-28 18:50:54 +0530

    LeetCode125

--- LC125.cpp
+++ LC125.cpp
@@ -1,28 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
-class Solution
-{
-public:
-    bool ispalin(string str)
-    {
+class Solution{
+    public:
+    bool ispalin(string str){
         string res;
-        for (char c : str)
-        {
-            if (isalnum(c))
-            {
+        for(char c: str){
+            if(isalnum(c)){
                 res.push_back(tolower(c));
             }
         }
         return cp(res);
     }
-    bool cp(string &res)
-    {
-        int i = 0;
-        int j = res.size() - 1;
-        while (i < j)
-        {
-            if (res[i] != res[j])
-            {
+    bool cp(string& res){
+        int i =0;
+        int j = res.size()-1;
+        while(i<j){
+            if(res[i]!= res[j]){
                 return false;
             }
             i++;
