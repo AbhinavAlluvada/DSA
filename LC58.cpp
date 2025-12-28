d6f9f882824654b277a0e3764e07e03c228a0a6c
AbhinavAlluvada <abhinav.alluvada@gmail.com>
2025-12-28 19:27:57 +0530

    LeetCode58

--- LC58.cpp
+++ LC58.cpp
@@ -4,19 +4,11 @@ using namespace std;
 class Solution {
 public:
     int lengthOfLastWord(string s) {
-        int n = s.size();
-        int count = 0;
-        int i = n-1;
-        if(n==1){
-            return 1;
-        }
-        while(i>0){
-            if(isalnum(s[i])){
-                count++;
-                if(!isalnum(s[i-1])){
-                    break;
-                } 
-            }
+        int i = s.size()-1;
+        int count= 0;
+        while(i>=0 && s[i]==' '){i--;}
+        while(i>=0 && s[i]!=' '){
+            count++;
             i--;
         }
         return count;
@@ -25,7 +17,7 @@ public:
 
 int main() {
     Solution sol;
-    string s = "a";
-    cout << sol.lengthOfLastWord(s);
+    string s = "a ";
+    cout << sol.lengthOfLastWord(s)<<endl;
     return 0;
 }
