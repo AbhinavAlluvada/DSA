7ab7e26c41ac0a6b577f85a8051f3ef2d3a50f57
AbhinavAlluvada <abhinav.alluvada@gmail.com>
2025-12-30 19:05:36 +0530

    LeetCode03

--- LC03.cpp
+++ LC03.cpp
@@ -1,26 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
-class Solution{
-    public:
-    int longestsubstr(string str){
-        int n = str.size();
-        int left =0 , right = 0 , maxlen =0;
-        vector <int> vect (256,0);
-        while(left<n){
-            if(right < n && vect[str[right]] == 0){
-                vect[str[right++]]++;
-                maxlen = max(maxlen, right -left);
-            }
-            else{
-                vect[str[left++]]--;
+
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        vector<int> vect(256, 0);
+        int n = s.size();
+        int maxlen = 0;
+        int right = 0;
+        int left = 0;
+
+        while (left < n) {
+            if (right < n && vect[s[right]] == 0) {
+                vect[s[right++]]++;
+                maxlen = max(maxlen, right - left);
+            } else {
+                vect[s[left++]]--;
             }
         }
         return maxlen;
     }
 };
-int main(){
-    Solution obj;
-    string str = "";
-    cout << obj.longestsubstr(str)<<endl;
+
+int main() {
+    Solution sol;
+    string s;
+    cin >> s;
+    cout << sol.lengthOfLongestSubstring(s);
     return 0;
 }
