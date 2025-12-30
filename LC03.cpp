f513fad4de9fcefec1b88fc1504ae474976877fa
AbhinavAlluvada <abhinav.alluvada@gmail.com>
2025-12-30 19:02:59 +0530

    LeetCode03

---
+++ LC03.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution{
+    public:
+    int longestsubstr(string str){
+        int n = str.size();
+        int left =0 , right = 0 , maxlen =0;
+        vector <int> vect (256,0);
+        while(left<n){
+            if(right < n && vect[str[right]] == 0){
+                vect[str[right++]]++;
+                maxlen = max(maxlen, right -left);
+            }
+            else{
+                vect[str[left++]]--;
+            }
+        }
+        return maxlen;
+    }
+};
+int main(){
+    Solution obj;
+    string str = "pwwkew";
+    cout << obj.longestsubstr(str)<<endl;
+    return 0;
+}
